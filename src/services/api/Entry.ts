3c624822832c42431215e7d19962fbbbc5ca6b45
Anton Yanchuk <anton@yanchuk.name>
2021-11-07 23:10:38 +0300

    Removed old client code

--- src/services/api/Entry.ts
+++ src/services/api/Entry.ts
@@ -1,28 +1,30 @@
-import ApiClient from 'services/ApiClient';
-import { IEntryForCreate, IEntryForUpdate, IEntryRaw } from 'types/entry';
+import { IEntry, IEntryForCreate, IEntryForUpdate, IEntryRaw } from 'types/entry';
 import { TId, TResult } from 'types/app';
-import { AxiosResponse } from 'axios';
-import { SuccessResponse } from 'utils/response';
 import EntryEntity from 'entities/EntryEntity';
-import _isArray from 'lodash/isArray';
+import PrimaryClient from 'services/PrimaryClient';
+import ResponseDto from 'services/ResponseDto';
 
-const handleSuccess = (response: AxiosResponse<IEntryRaw | IEntryRaw[]>) => {
-  const data = _isArray(response.data) ? response.data.map(i => EntryEntity.decrypt(i)) : EntryEntity.decrypt(response.data);
-  return new SuccessResponse(data);
+const api = PrimaryClient.getClient();
+const decodeAtomCollectionResponse = (response: ResponseDto<IEntryRaw[]>) => {
+  const data = response.result.map(i => EntryEntity.decrypt(i));
+  return Promise.resolve(new ResponseDto<IEntry[]>(data));
+};
+const decodeAtomResponse = (response: ResponseDto<IEntryRaw>) => {
+  return Promise.resolve(new ResponseDto<IEntry>(EntryEntity.decrypt(response.result)));
 };
 
-export const fetchStarred = () => ApiClient.takeGet<IEntryRaw[]>('/atoms/starred').then(handleSuccess).catch(ApiClient.handleError);
+export const fetchStarred = () => api().get<IEntryRaw[]>('/atoms/starred').then(decodeAtomCollectionResponse);
 
-export const fetchLatest = () => ApiClient.takeGet<IEntryRaw[]>('/atoms/latest').then(handleSuccess).catch(ApiClient.handleError);
+export const fetchLatest = () => api().get<IEntryRaw[]>('/atoms/latest').then(decodeAtomCollectionResponse);
 
-export const fetchByGroup = (groupId: TId) => ApiClient.takeGet<IEntryRaw[]>(`/atoms/group/${groupId}`).then(handleSuccess).catch(ApiClient.handleError);
+export const fetchByGroup = (groupId: TId) => api().get<IEntryRaw[]>(`/atoms/group/${groupId}`).then(decodeAtomCollectionResponse);
 
-export const fetchById = (entryId: TId) => ApiClient.takeGet<IEntryRaw>(`/atoms/${entryId}`).then(handleSuccess).catch(ApiClient.handleError);
+export const fetchById = (entryId: TId) => api().get<IEntryRaw>(`/atoms/${entryId}`).then(decodeAtomResponse);
 
-export const doUpdate = (data: IEntryForUpdate) => ApiClient.put<TResult>(`/atoms/${data.id}`, EntryEntity.encrypt(data));
+export const doUpdate = (data: IEntryForUpdate) => api().put<TResult>(`/atoms/${data.id}`, EntryEntity.encrypt(data));
 
-export const doCreate = (data: IEntryForCreate) => ApiClient.post<TResult>('/atoms', EntryEntity.encrypt(data));
+export const doCreate = (data: IEntryForCreate) => api().post<TResult>('/atoms', EntryEntity.encrypt(data));
 
-export const doRemove = (entryId: TId) => ApiClient.delete<TResult>(`/atoms/${entryId}`);
+export const doRemove = (entryId: TId) => api().delete<TResult>(`/atoms/${entryId}`);
 
-export const doStarred = (entryId: TId) => ApiClient.put<TResult>(`/atoms/starred/${entryId}`);
+export const doStarred = (entryId: TId) => api().put<TResult>(`/atoms/starred/${entryId}`, {});
