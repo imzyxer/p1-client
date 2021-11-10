72625bba103b9cd2f8a1920eb78a78937d9e02c1
Anton Yanchuk <anton@yanchuk.name>
2021-11-10 21:26:24 +0300

    Renamed Entry endpoints

--- src/services/api/Entry.ts
+++ src/services/api/Entry.ts
@@ -13,18 +13,18 @@ const decodeAtomResponse = (response: ResponseDto<IEntryRaw>) => {
   return Promise.resolve(new ResponseDto<IEntry>(EntryEntity.decrypt(response.result)));
 };
 
-export const fetchStarred = () => api().get<IEntryRaw[]>('/atoms/starred').then(decodeAtomCollectionResponse);
+export const fetchStarred = () => api().get<IEntryRaw[]>('/things/starred').then(decodeAtomCollectionResponse);
 
-export const fetchLatest = () => api().get<IEntryRaw[]>('/atoms/latest').then(decodeAtomCollectionResponse);
+export const fetchLatest = () => api().get<IEntryRaw[]>('/things/latest').then(decodeAtomCollectionResponse);
 
-export const fetchByGroup = (groupId: TId) => api().get<IEntryRaw[]>(`/atoms/group/${groupId}`).then(decodeAtomCollectionResponse);
+export const fetchByGroup = (groupId: TId) => api().get<IEntryRaw[]>(`/things/group/${groupId}`).then(decodeAtomCollectionResponse);
 
-export const fetchById = (entryId: TId) => api().get<IEntryRaw>(`/atoms/${entryId}`).then(decodeAtomResponse);
+export const fetchById = (entryId: TId) => api().get<IEntryRaw>(`/things/${entryId}`).then(decodeAtomResponse);
 
-export const doUpdate = (data: IEntryForUpdate) => api().put<TResult>(`/atoms/${data.id}`, EntryEntity.encrypt(data));
+export const doUpdate = (data: IEntryForUpdate) => api().put<TResult>(`/things/${data.id}`, EntryEntity.encrypt(data));
 
-export const doCreate = (data: IEntryForCreate) => api().post<TResult>('/atoms', PrimaryClient.toFormData(EntryEntity.encrypt(data)));
+export const doCreate = (data: IEntryForCreate) => api().post<TResult>('/things', PrimaryClient.toFormData(EntryEntity.encrypt(data)));
 
-export const doRemove = (entryId: TId) => api().delete<TResult>(`/atoms/${entryId}`);
+export const doRemove = (entryId: TId) => api().delete<TResult>(`/things/${entryId}`);
 
-export const doStarred = (entryId: TId) => api().put<TResult>(`/atoms/starred/${entryId}`, {});
+export const doStarred = (entryId: TId) => api().put<TResult>(`/things/starred/${entryId}`, {});
