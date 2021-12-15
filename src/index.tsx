42551fc9a20831085f5e9901779898f25f68e533
Anton Yanchuk <anton@yanchuk.name>
2021-12-15 19:24:03 +0300

    Added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,20 @@ import Wrapper from 'components/App/Wrapper';
 import { UseRootStore } from 'stores/hooks/useRootStore';
 import PrimaryAuthenticator from 'services/PrimaryAuthenticator';
 import PrimaryClient from 'services/PrimaryClient';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  const sentryDsn = process.env.REACT_APP_SENTRY_DSN ?? null;
+  if (sentryDsn !== null) {
+    Sentry.init({
+      dsn: sentryDsn,
+      integrations: [new Integrations.BrowserTracing()],
+      tracesSampleRate: 1.0,
+    });
+  }
+}
 
 const baseURL = process.env.REACT_APP_API_HOST ?? '';
 const authenticator = new PrimaryAuthenticator();
