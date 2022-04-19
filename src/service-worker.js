094b43ae7faa5b3dde61591409023123997f3cd2
Anton Yanchuk <anton@yanchuk.name>
2022-04-19 21:38:56 +0300

    Service worker install listener

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,6 @@
 const ignored = self.__WB_MANIFEST;
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', () => {
+  console.log('sw', 'install');
+});
