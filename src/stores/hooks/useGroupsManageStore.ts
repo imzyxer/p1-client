c92e74e585b577bc2904b92a0a09ec4c6772ff15
Anton Yanchuk <anton@yanchuk.name>
2022-06-07 22:35:03 +0300

    Clean up

--- src/stores/hooks/useGroupsManageStore.ts
+++
@@ -1,6 +0,0 @@
-import useRootStore from 'stores/hooks/useRootStore';
-
-export default function useGroupsManageStore() {
-  const { groupsManageStore } = useRootStore();
-  return groupsManageStore;
-}
