ebbcdb86fdb2d7ee83d3d20fbcf36adb6bc89fac
Anton Yanchuk <anton@yanchuk.name>
2022-06-22 09:34:35 +0300

    Clean up

---
+++ src/stores/DialogStore.ts
@@ -0,0 +1,47 @@
+import { action, computed, makeObservable, observable } from 'mobx';
+
+type TParams = {
+  [key: string]: any;
+};
+
+class DialogStore {
+  @observable params: TParams = {};
+
+  constructor() {
+    makeObservable(this);
+    const p = new URLSearchParams(window.location.hash.replace(/^#/, ''));
+    if (p.has('dialog')) {
+      this.setParams(Object.fromEntries(p));
+    }
+  }
+
+  @action
+  public open = (dialog: string, params?: TParams) => {
+    const p = new URLSearchParams(params);
+    p.set('dialog', dialog);
+    this.setParams(Object.fromEntries(p));
+    window.location.hash = p.toString();
+  };
+
+  @action
+  public setParams = (params: TParams) => {
+    if (params.dialog) {
+      this.params = params;
+    } else {
+      this.close();
+    }
+  };
+
+  @action
+  public close = () => {
+    this.params = {};
+  };
+
+  @computed
+  get dialog() {
+    return this.params?.dialog || null;
+  }
+}
+
+export default DialogStore;
+export type TDialogStore = DialogStore;
--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,15 +1,16 @@
+import { createContext } from 'react';
 import AppStore, { TAppStore } from 'stores/AppStore';
 import RefsStore, { TRefsStore } from 'stores/RefsStore';
 import DashboardStore, { TDashboardStore } from 'stores/DashboardStore';
 import GroupViewStore, { TGroupViewStore } from 'stores/GroupViewStore';
-import { createContext } from 'react';
 import ThingViewStore, { TThingViewStore } from 'stores/ThingViewStore';
 import ThingEditStore, { TThingEditStore } from 'stores/ThingEditStore';
 import ThingAddStore, { TThingAddStore } from 'stores/ThingAddStore';
 import HistoryStore, { THistoryStore } from 'stores/HistoryStore';
 import GroupsManageStore, { TGroupsManageStore } from 'stores/GroupsManageStore';
 import ProfileEditStore, { TProfileEditStore } from 'stores/ProfileEditStore';
-import StandaloneStore, { TStandaloneStore } from './StandaloneStore';
+import StandaloneStore, { TStandaloneStore } from 'stores/StandaloneStore';
+import DialogStore, { TDialogStore } from 'stores/DialogStore';
 
 export default class RootStore {
   public appStore: TAppStore;
@@ -23,6 +24,7 @@ export default class RootStore {
   public thingAddStore: TThingAddStore;
   public historyStore: THistoryStore;
   public profileEditStore: TProfileEditStore;
+  public dialogStore: TDialogStore;
 
   constructor() {
     this.appStore = new AppStore(this);
@@ -36,6 +38,7 @@ export default class RootStore {
     this.thingEditStore = new ThingEditStore(this);
     this.thingAddStore = new ThingAddStore(this);
     this.profileEditStore = new ProfileEditStore(this);
+    this.dialogStore = new DialogStore();
   }
 }
 
